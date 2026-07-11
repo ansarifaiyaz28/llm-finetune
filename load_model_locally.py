c00ed31bdc12a2789dbb20db0a44dded838561af
ansarifaiyaz28 <ansari.faiyaz28@gmail.com>
2026-07-11 17:19:52 -0400

    added some files

---
+++ load_model_locally.py
@@ -0,0 +1,24 @@
+import os
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+MODEL_DIR=/home/user/...
+model_path = os.environ["MODEL_DIR"]
+
+tokenizer = AutoTokenizer.from_pretrained(
+    model_path,
+    local_files_only=True,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,
+    torch_dtype=torch.bfloat16,
+    local_files_only=True,
+)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+print("Model loaded from:", model_path)
+print("Device:", next(model.parameters()).device)
